e42503b9b328ee5760820013605adcdc619892f0
lucasmcar <lm.lucas90@gmail.comr>
2026-09-23 11:26:03 -0300

    Remove .nuxt do versionamento

--- .nuxt/types/supabase.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '#supabase/server' {
-  const serverSupabaseClient: typeof import('/var/www/html/code-experts-site-vue/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseClient
-  const serverSupabaseServiceRole: typeof import('/var/www/html/code-experts-site-vue/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseServiceRole
-  const serverSupabaseUser: typeof import('/var/www/html/code-experts-site-vue/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseUser
-  const serverSupabaseSession: typeof import('/var/www/html/code-experts-site-vue/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseSession
-}
